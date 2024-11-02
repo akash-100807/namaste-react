dc4859bd6ee32b991919237a36aeafc62a3969ff
akash-100807 <akash100807@gmail.com>
2024-11-02 16:49:19 +0530

    episode 1 version 2

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
-
+import React from "react";  //These two are coming from node_modules
+import ReactDOM from "react-dom/client";
 
 let parent = React.createElement("div", {id : "parent"}, [
     React.createElement("div", {id : "child1"}, [
-        React.createElement("h1", {id : "heading1"}, "This is h1 tag"),
+        React.createElement("h1", {id : "heading1"}, "Namaste React🚀"),
         React.createElement("h2", {id : "heading2"}, "This is h2 tag")
     ]),
     React.createElement("div", {id : "child1"}, [
-        React.createElement("h1", {id : "heading1"}, "This is h1 tag"),
+        React.createElement("h1", {id : "heading1"}, "This is heading tag"),
         React.createElement("h2", {id : "heading2"}, "This is h2 tag")
     ])
 ])
 
+console.log(parent);
 let root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-#React Course
+# Namaste React 🚀😎
+
+# Parcel - Bundler
+ - Dev Build
+ - Local Server
+ - HMR = Hot Module Replacement
+ - File Watching Algorithm - written in C++
+ - Caching things - For Faster Builds
+ - Does Image Optimization Also
+ - Minification of the files for Production
+ - Does Bundling
+ - Compressing is also done
+ - Consistent Hashing***
+ - Code Splitting
+ - Differential Bundling - Supporting for older browsers
+ - Diagnostic
+ - Error Handling
+ - Ability to work with HTTPs also!
+ - Tree Shaking Algorithm = Remove unused code.
+ - Different dev and prod bundles
